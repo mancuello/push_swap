278502cab918ac6b1c76af5438b7ca8cc879e8d0
Manuel Cuello <mcuello@c4r4s2.42urduliz.com>
2025-04-14 15:12:55 +0200

    adding handler

--- list_handler.c
+++ list_handler.c
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   list_handler.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcuello <mcuello@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/14 11:16:32 by mcuello           #+#    #+#             */
-/*   Updated: 2025/04/14 11:20:48 by mcuello          ###   ########.fr       */
+/*   Created: 2025/04/14 11:29:24 by mcuello           #+#    #+#             */
+/*   Updated: 2025/04/14 11:45:01 by mcuello          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-#include <unistd.h>
-
-typedef struct s_list
+t_list	*ft_lstnew(int n)
 {
-	int		data;
-	s_list	*next;
-}	t_list;
+	t_list *head;
 
-#endif
+	head = malloc(sizeof(t_list));
+	if (!head)
+		perror("Fallo en la asignacion de memoria");
+	head->data = n;
+	head->next = NULL;
+	return (head);
+}
--- main.c
+++ main.c
@@ -6,7 +6,30 @@
 /*   By: mcuello <mcuello@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 17:48:51 by mcuello           #+#    #+#             */
-/*   Updated: 2025/04/13 18:15:02 by mcuello          ###   ########.fr       */
+/*   Updated: 2025/04/14 11:54:34 by mcuello          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_list	*stack_b;
+	void	*head_dir;
+	unsigned int	i = 1;
+
+	if (argc == 1)
+		return (1);
+	else
+	{
+		while (argv[i])
+		{
+			stack_b = ft_lstnew(ft_atoi(argv[i++]));
+			if (i == 2)
+				head_dir = stack_b;
+			stack_b = stack_b->next;
+		}
+		stack_b = head_dir;
+	}
+	return (0);
+}
